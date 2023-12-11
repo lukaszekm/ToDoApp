b80984a638336c8f9c43c633993f0f88424e1241
Lukasyno <lukasz9610@gmail.com>
2023-12-11 20:54:02 +0100

    Create basic GUI with no interactions at all

---
+++ day16_cli.py
@@ -0,0 +1,61 @@
+import time
+from functions import get_todos, write_todos
+
+
+filename = r'todos.txt'
+now = time.strftime("%d %b, %Y  %H:%M:%S")
+while True:
+    print(now)
+    user_message = input("Type add, show, edit, delete, clear, complete or exit:")
+
+    todos = get_todos()
+
+    if user_message.startswith("add") or user_message.startswith("new"):
+        todo = user_message[4:]
+        todos.append(todo + "\n")
+        write_todos(todos)
+
+    elif user_message.startswith("show"):
+        for index, item in enumerate(todos):
+            item = item.title()
+            print(f"{index + 1}. {item[:-1]}")
+        # new_todos = [item.strip('\n') for item in todos]
+    elif user_message.startswith("complete"):
+        complete_id = int(user_message[9]) - 1
+        todos[complete_id] += " - check"
+
+        write_todos(todos)
+
+    elif user_message.startswith("edit"):
+        try:
+            edit_id = int(user_message[5]) - 1
+            edit = input(f"Editing {str(todos[edit_id])}:")
+            todos[edit_id] = edit + '\n'
+
+            write_todos(todos)
+
+        except ValueError:
+            print("Your command is invalid!")
+            continue
+
+    elif user_message.startswith("clear"):
+        todos.clear()
+
+    elif user_message.startswith("delete"):
+        try:
+            delete_id = int(user_message[7]) - 1
+            to_delete = todos[delete_id].strip('\n')
+            todos.pop(delete_id)
+            print(f"{to_delete} removed succesfully!")
+
+            write_todos(todos)
+
+        except IndexError:
+            print("Invalid Number!")
+            continue
+
+    elif user_message.startswith("exit"):
+        print("Bajo!")
+        break
+    else:
+        print("Unknown command!")
---
+++ gui.py
@@ -0,0 +1,13 @@
+import functions
+import PySimpleGUI as gui
+
+
+todo_label = gui.Text("Enter a To-Do: ")
+input_box = gui.InputText(tooltip="Enter To-Do")
+add_button = gui.Button("Add")
+edit_button = gui.Button("Edit")
+
+
+main_window = gui.Window('To-Do App', layout=[[todo_label], [input_box, add_button], [edit_button]])
+main_window.read()
+main_window.close()
